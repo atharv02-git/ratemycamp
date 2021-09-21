87dd12a17ec94154ef9b023ce767b89f5f81b21f
Atharv <atharvbhandare2001@gmail.com>
2021-09-21 20:14:27 +0530

    Served Static files

--- app.js
+++ app.js
@@ -30,9 +30,10 @@ const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/campgrounds', campgroundRoutes)
