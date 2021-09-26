ac57a6de3b7ffdbf97ed20e3f8778ce09cce59cc
Atharv <atharvbhandare2001@gmail.com>
2021-09-26 16:08:52 +0530

    implemented static methods

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi')
-const { campgroundSchema, reviewSchema } = require('./ErrorSchemas')
-const session = require('express-session')
-const flash = require('connect-flash')
+const Joi = require('joi');
+const { campgroundSchema, reviewSchema } = require('./ErrorSchemas');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const ExpressError = require('./utils/ExpressError')
 const CatchAsync = require('./utils/CatchAsync')
@@ -51,10 +54,26 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+// middleware for passport
+app.use(passport.initialize());
+app.use(passport.session());
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+// creating fake route toregister user
+app.get('/fakeUser', async(req, res) => {
+    const user = new User({ email: 'atharv@gmail.com', username: 'atharrrvvv' });
+    const newUser = await User.register(user, 'qwerty');
+    res.send(newUser);
 })
 
 app.use('/campgrounds', campgroundRoutes)
