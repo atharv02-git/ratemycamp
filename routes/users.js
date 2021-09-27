b6db29cd579eb492eb761c3a4b3815642e472f9f
Atharv <atharvbhandare2001@gmail.com>
2021-09-27 18:38:02 +0530

    Login routes & validation added

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport')
 const catchAsync = require('../utils/CatchAsync')
 const User = require('../models/user')
 
+// User Register get & post route:
 router.get('/register', (req, res) => {
     res.render('users/register');
 })
-
 router.post('/register', catchAsync(async(req, res) => {
     try {
         const { email, username, password } = req.body;
@@ -19,5 +20,16 @@ router.post('/register', catchAsync(async(req, res) => {
         res.redirect('register')
     }
 }));
+
+// User Login get and post route:
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res) => {
+    const { username } = req.body;
+    req.flash('success', `Welcome back, ${username}!`);
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
