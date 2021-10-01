e0164ef51d04fa1236940d05314119a5daf449ce
Atharv <atharvbhandare2001@gmail.com>
2021-10-01 22:23:05 +0530

    Star Rating added

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,7 +1,7 @@
 const Campground = require('../models/campground');
 
 module.exports.index = async(req, res) => {
-    const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find({})
     res.render('campgrounds/index', { campgrounds })
 }
 
